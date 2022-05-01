11c0df1d93f65c4ffdfe7df37cfbe316f82a865b
monapavasiya <mona.pavasiya08@gmail.com>
2022-05-01 16:51:49 -0400

    added new pacakage day3 and 3 classes

---
+++ JavaProjectAM/src/day3/EvenOrOddNumber.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class EvenOrOddNumber 
+{
+
+	public static void main(String[] args) 
+	{
+		int num = 5;
+		
+		if(num%2 == 0)
+		{
+			System.out.println("Number is Even number");
+		}
+		else
+		{
+			System.out.println("Number is Odd number");
+		}
+	
+	}
+
+}
---
+++ JavaProjectAM/src/day3/IfElseCondiion.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class IfElseCondiion {
+
+	public static void main(String[] args) 
+	{
+		int age = 15;
+
+		if(age >= 18) 
+		{
+			System.out.println("Eligible for vote");
+		}
+		else
+		{
+			System.out.println("Not eligible for vote");
+		}
+		
+
+	}
+
+}
